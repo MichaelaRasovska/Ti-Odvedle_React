5cedd98b5899b04eeff4fa69a3404fef7ff56723
Michaela Rasovska <michaela.rasovska@gmail.com>
2020-11-24 11:22:34 +0100

    Úprava struktury hlavní stránky

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Catalogue } from './components/catalogue/index.jsx';
 import { Form } from './components/form/index.jsx';
-import { Map } from './components/map/index.jsx'
+import { Map } from './components/map/index.jsx';
 import './index.html';
 
 const App = () => {
   return (
     <>
-      {/* tady bude mapa */}
-      <Catalogue />
       <Form />
+      <p class="map__item">
+        Objevte naší interaktivní mapu a podívejte se kdo ve vašem okolí zrovna
+        potřebuje pomoc:
+      </p>
+      <Catalogue />
       <Map />
     </>
   );

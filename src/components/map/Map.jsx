01ba992ebbe86c30cecbba0be6fe22175700fceb
Michaela Rasovska <michaela.rasovska@gmail.com>
2020-11-30 19:26:15 +0100

    Oprava komponenty mapy

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -38,58 +38,65 @@ export const Map = () => {
 
   return (
     <>
-      <label>
-        Chci pomáhat:
-        <select
-          className="map-select"
-          value={filter}
-          onChange={(event) => setFilter(event.target.value)}
-        >
-          {' '}
-          <option value="">Vyberte</option>
-          <option value="Materiální pomoc">materiálně</option>
-          <option value="Fyzická pomoc">fyzicky</option>
-          <option value="Odvoz">s odvozem</option>
-          <option value="Jiné">jinak</option>
-        </select>
-      </label>
-      <div id="map" className="map">
-        <ReactMapGL
-          {...viewport}
-          onViewportChange={(nextViewport) => setViewport(nextViewport)}
-          width="80%"
-          height={500}
-          mapboxApiAccessToken="pk.eyJ1IjoidGlvZHZlZGxlIiwiYSI6ImNraHV2NWw3bjBvaHMycnA1aDJvenY3YjYifQ.84cTo_cZbhjqOJ7r-BveXw"
-          mapStyle={seznamMapy}
-        >
-          <div className="map-navigation">
-            <NavigationControl />
-            <GeolocateControl />
-          </div>
-          {dataFiltered.map((person) => {
-            return (
-              <Marker
-                key={person.id}
-                person={person}
-                setPopupData={setPopupData}
+      <section id="map-section" className="map-section">
+        <h2 className="map-heading">Interaktivní mapa pomoci</h2>
+        <p id="map-item" className="map__item">
+          Objevte naší interaktivní mapu a podívejte se kdo ve vašem okolí
+          zrovna potřebuje pomoc.
+        </p>
+        <label>
+          Chci pomáhat:
+          <select
+            className="map-select"
+            value={filter}
+            onChange={(event) => setFilter(event.target.value)}
+          >
+            {' '}
+            <option value="">Vyberte</option>
+            <option value="Materiální pomoc">materiálně</option>
+            <option value="Fyzická pomoc">fyzicky</option>
+            <option value="Odvoz">s odvozem</option>
+            <option value="Jiné">jinak</option>
+          </select>
+        </label>
+        <div className="map">
+          <ReactMapGL
+            {...viewport}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            width="80%"
+            height={500}
+            mapboxApiAccessToken="pk.eyJ1IjoidGlvZHZlZGxlIiwiYSI6ImNraHV2NWw3bjBvaHMycnA1aDJvenY3YjYifQ.84cTo_cZbhjqOJ7r-BveXw"
+            mapStyle={seznamMapy}
+          >
+            <div className="map-navigation">
+              <NavigationControl />
+              <GeolocateControl />
+            </div>
+            {dataFiltered.map((person) => {
+              return (
+                <Marker
+                  key={person.id}
+                  person={person}
+                  setPopupData={setPopupData}
+                />
+              );
+            })}
+            {popupData !== null && (
+              <Popup
+                latitude={popupData.latitude}
+                longitude={popupData.longitude}
+                closeOnClick={false}
+                onClose={() => setPopupData(null)}
+                onClick={() => setPopupData(null)}
+                age={popupData.age}
+                name2={popupData.name2}
+                description={popupData.description}
+                id={popupData.id}
               />
-            );
-          })}
-          {popupData !== null && (
-            <Popup
-              latitude={popupData.latitude}
-              longitude={popupData.longitude}
-              closeOnClick={false}
-              onClose={() => setPopupData(null)}
-              onClick={() => setPopupData(null)}
-              age={popupData.age}
-              name2={popupData.name2}
-              description={popupData.description}
-              id={popupData.id}
-            />
-          )}
-        </ReactMapGL>
-      </div>
+            )}
+          </ReactMapGL>
+        </div>
+      </section>
     </>
   );
 };
